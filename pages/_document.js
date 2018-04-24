b2fbb3127e6377162bdf8eef48e6551af81d3cc8
Cole <eloc49@gmail.com>
2018-04-24 15:55:34 -0400

    fixed favicon

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ export default class ColeDocument extends Document {
             content="Cole Inman Full Stack Software Developer Resume and Project showcase"/>
           <link href="https://fonts.googleapis.com/css?family=Open+Sans|Lobster" rel="stylesheet"/>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
-          <link rel="shortcut icon" href="/favicon.ico"/>
+          <link rel="shortcut icon" href="/static/favicon.ico"/>
           {this.props.styleTags}
         </Head>
 
